c8a08537297b20adee926889893228ca7dfed0ec
Farru-khan <kfarru171@gmail.com>
2024-05-26 12:29:20 +0430

    Initial

--- src/PersonalComp/ExpenseTracker.js
+++ src/PersonalComp/ExpenseTracker.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ExpenseTracker = () => {
+  const [title, setTitle] = useState('')
+  const [desp, setDesp] =useState('')
+  const [amount, setAmount]= useState('')
   return (
-    <div>
+    <>
     <h2>Expense</h2>
-    </div>
+    <section className='content'>
+      <div className='row'>
+        <input type='text' value={title} onChange={e=>setTitle(e.target.value)} placeholder='expense-Title'/>
+      </div>
+      <div className='row' value={desp} onChange={e=>setDesp(e.target.value)}>
+        <input type='text' placeholder='Expense-description'/>
+      </div>
+      <div className='row'>
+        <input type='text' value={amount} onChange={e=>setAmount(e.target.value)} placeholder='Total-amount'/>
+      </div>
+      <div>
+        <button className='track'>Add Expense</button>
+      </div>
+    </section>
+    </>
   )
 }
 
