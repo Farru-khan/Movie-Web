d70a20cf23333c1360251081f694cf2647206520
Farru-khan <kfarru171@gmail.com>
2024-06-21 19:03:44 +0430

    Intial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PersonalUse from './PersonalUse'
+import RecipeApp from './RecipeApp'
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    <PersonalUse/>
+     <RecipeApp/>
     </BrowserRouter>
 );
