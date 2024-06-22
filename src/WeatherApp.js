614f7ea6ab6c5fe490807b4323d5f094fd039625
Farru-khan <kfarru171@gmail.com>
2024-06-22 15:02:33 +0430

    first commit

---
+++ src/WeatherApp.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weather from './WeatherCom/Weather';
+
+function WeatherApp() {
+  return (
+    <div className="App">
+    <Weather></Weather>
+    </div>
+  );
+}
+
+export default WeatherApp;
